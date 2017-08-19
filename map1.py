081805574d1ec19fd01f89fb8e9c0c52fa98901f
Masya120 <maxk893@gmail.com>
2017-08-18 19:01:24 -0700

    Changed for loop in map1.py to use zip function instead of iterating using index

--- map1.py
+++ map1.py
@@ -1,7 +1,7 @@
 import folium
 import pandas
 
-map = folium.Map(location=[47.586160, -122.308046], zoom_start=9)
+map = folium.Map(location=[47.586160, -122.308046], zoom_start=10)
 
 fg = folium.FeatureGroup(name="My Map")
 
@@ -11,8 +11,8 @@ location = list(data["Names"])
 lat = list(data["Lat"])
 lon = list(data["Lon"])
 
-for place in range(0, len(location)):
-    fg.add_child(folium.Marker(location=[lat[place], lon[place]], popup="%s" % location[place], icon=folium.Icon(color='green')))
+for lc, lt, ln in zip(location, lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="%s" % lc, icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
