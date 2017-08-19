f5cbd9d1ebe36240a554509073a9672cfd179bbf
Masya120 <maxk893@gmail.com>
2017-08-19 09:01:01 -0700

    Created a polygon layer over the map. Layer shows different color based on pop of country

--- map1.py
+++ map1.py
@@ -1,7 +1,7 @@
 import folium
 import pandas
 
-map = folium.Map(location=[47.586160, -122.308046], zoom_start=10)
+map = folium.Map(location=[47.586160, -122.308046], zoom_start=7)
 
 fg = folium.FeatureGroup(name="My Map")
 
@@ -13,6 +13,9 @@ lon = list(data["Lon"])
 
 for lt, ln, lc in zip(lat, lon, location):
     fg.add_child(folium.Marker(location=[lt, ln], popup="%s" % lc, icon=folium.Icon(color='green')))
+
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'), 
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 1000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
 
